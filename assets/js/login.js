f47b489b2df333ba1cf471b72882012d65aaae13
itheima <452617928@qq.com>
2020-09-02 15:43:27 +0800

    dashijian

--- assets/js/login.js
+++ assets/js/login.js
@@ -31,4 +31,23 @@ $(function() {
         }
     })
 
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+            // 2. 发起Ajax的POST请求
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+                // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+
 })
